c2a9315a74d0e7a651f15fecbd0dfaefc33ebc4b
EC2 Default User <ec2-user@ip-172-31-36-243.eu-west-1.compute.internal>
2023-09-05 09:02:17 +0000

    aha, fix

--- src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
+++ src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
@@ -21,7 +21,7 @@ import static java.util.Optional.ofNullable;
  * NO MODIFICATION OF THIS CLASS IS ALLOWED!
  */
 @Component
-class ReallyShakyBankingCoreSystemService implements-dog BankingCoreSystmeService {
+class ReallyShakyBankingCoreSystemService implements BankingCoreSystmeService {
 
     private Map<String, Account> theBank = new HashMap();
 
